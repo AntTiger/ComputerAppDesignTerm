cfbb14c1048ac6029cd455a703d633b02df7f501
gxicxigouxa <j6wyfnea>
2017-05-31 00:03:36 +0900

    Create MyLibraryActivity, add listener of myLibrary Button.

--- app/src/main/java/com/example/kjw/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/kjw/mylibrary/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.kjw.mylibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button myLibraryCallButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myLibraryCallButton = (Button)findViewById(R.id.my_library_button);
+        myLibraryCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyLibraryActivity.class);
+                //intent.putExtra("id", "201212345");
+                //intent.putExtra("password", "password");
+                startActivity(intent);
+            }
+        });
     }
 }
 //test.
