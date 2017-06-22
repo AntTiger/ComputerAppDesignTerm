f480cfb0e2855a0a92018c8fb658504ae69f45d6
kbbn2001 <wkddns1@naver.com>
2017-06-22 21:49:03 +0900

    add search function
    implementation search wirh keyword

--- app/src/main/java/com/example/kjw/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/kjw/mylibrary/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kjw.mylibrary;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("main", "start");
 
         myLibraryCallButton = (Button)findViewById(R.id.my_library_button);
         myLibraryCallButton.setOnClickListener(new View.OnClickListener() {
