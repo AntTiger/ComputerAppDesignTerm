c6caeb75b2c9030f0d22bbc10cc105e6b222cfc4
LHS <ehfdlgus@gmail..com>
2017-06-26 15:33:51 +0900

    studyroom final

--- app/src/main/java/com/example/kjw/mylibrary/StudyroomActivity.java
+++ app/src/main/java/com/example/kjw/mylibrary/StudyroomActivity.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StudyroomActivity extends AppCompatActivity {
+
+    private TextView sr1name;
+    private TextView sr2name;
+    private TextView sr3name;
+    private TextView sr4name;
 
     private Button sr1Button;
     private Button sr2Button;
@@ -17,12 +23,19 @@ public class StudyroomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studyroom);
+
+        sr1name=(TextView)findViewById(R.id.sr1_name);
+        sr2name=(TextView)findViewById(R.id.sr2_name);
+        sr3name=(TextView)findViewById(R.id.sr3_name);
+        sr4name=(TextView)findViewById(R.id.sr4_name);
+
         sr1Button = (Button)findViewById(R.id.sr1_button);
         sr1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StudyroomActivity.this, StudyroomDetailActivity.class);
                 intent.putExtra("sr",1);
+                intent.putExtra("sn",sr1name.getText().toString());
                 startActivity(intent);
             }
         });
@@ -32,6 +45,7 @@ public class StudyroomActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(StudyroomActivity.this, StudyroomDetailActivity.class);
                 intent.putExtra("sr",2);
+                intent.putExtra("sn",sr2name.getText().toString());
                 startActivity(intent);
             }
         });
@@ -41,6 +55,7 @@ public class StudyroomActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(StudyroomActivity.this, StudyroomDetailActivity.class);
                 intent.putExtra("sr",3);
+                intent.putExtra("sn",sr3name.getText().toString());
                 startActivity(intent);
             }
         });
@@ -50,6 +65,7 @@ public class StudyroomActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(StudyroomActivity.this, StudyroomDetailActivity.class);
                 intent.putExtra("sr",4);
+                intent.putExtra("sn",sr4name.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/kjw/mylibrary/StudyroomDetailActivity.java
+++ app/src/main/java/com/example/kjw/mylibrary/StudyroomDetailActivity.java
@@ -1,24 +1,54 @@
 package com.example.kjw.mylibrary;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class StudyroomDetailActivity extends AppCompatActivity {
 
     private ArrayList<TextView> seat=new ArrayList<TextView>();
 
-    private int Roomnum;
+    private int roomnum;
+    private static final String TAG = "StudyroomDetailActivity";
+
+    private static final String TAG_JSON="studyroom";
+    private static final String TAG_SN ="seatnumber";
+
+    private TextView srname;
+
+    private String mJsonString;
+    private String roomname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studyroom_detail);
 
         Intent intent = getIntent();
-        Roomnum=intent.getIntExtra("sr",0);
+        roomnum =intent.getIntExtra("sr",0);
+        roomname = intent.getStringExtra("sn");
+
+        srname=(TextView)findViewById(R.id.roomname);
+        srname.setText(roomname);
+
+
 
         seat.add((TextView)findViewById(R.id.seat1));
         seat.add((TextView)findViewById(R.id.seat2));
@@ -46,5 +76,129 @@ public class StudyroomDetailActivity extends AppCompatActivity {
         seat.add((TextView)findViewById(R.id.seat22));
         seat.add((TextView)findViewById(R.id.seat23));
         seat.add((TextView)findViewById(R.id.seat24));
+
+
+        GetSeat task=new GetSeat();
+
+        task.execute(Integer.toString(roomnum));
+    }
+    private class GetSeat extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(StudyroomDetailActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+            }
+            else {
+
+                mJsonString = result;
+                seatchk();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = "http://"+ServerIpData.serverIp+"/get_studyroom_seat.php";
+            String roomnumber = (String)params[0];
+
+            String postParameters = "roomnumber=" + roomnumber ;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+    private void seatchk(){
+        try {
+
+
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String seatnumber = item.getString(TAG_SN);
+
+                int sn=Integer.valueOf(seatnumber);
+
+                seat.get(sn-1).setBackgroundColor(Color.GRAY);
+
+            }
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "seatchk : ", e);
+        }
+
     }
 }
